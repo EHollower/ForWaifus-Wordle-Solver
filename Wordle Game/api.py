e8c8fad4f65d2019ca1a6e8b98a557775fa520b2
EHollower <76710132+EHollower@users.noreply.github.com>
2022-10-27 22:25:12 +0300

    api for wordle game

---
+++ Wordle Game/api.py
@@ -0,0 +1,9 @@
+from copyreg import pickle
+import random
+from dictionary import wordle_dictionary 
+
+def main():
+      RanWord = random.choice(wordle_dictionary)
+
+if __name__ == '__main__':
+      main()
