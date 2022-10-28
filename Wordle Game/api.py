8e949c209d6009eefcc26a69705de57a3282a11e
EHollower <76710132+EHollower@users.noreply.github.com>
2022-10-28 23:44:50 +0300

    Update api.py

--- Wordle Game/api.py
+++ Wordle Game/api.py
@@ -1,5 +1,21 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Wordle")
+
 def main():
-      return
+      clock = pygame.time.Clock()
+      while True:
+            for event in pygame.event.get():
+                  if event.type == pygame.QUIT:
+                        pygame.quit()
+                        exit()
+            #draw all the elements required to make a wordle api
+            #update everything
+            pygame.display.update()
+            clock.tick(60)
 
 if __name__ == '__main__':
       main()
