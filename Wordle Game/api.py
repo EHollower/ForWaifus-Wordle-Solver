122200ca825ba708c4ae4f5507fe3f956571660b
EHollower <76710132+EHollower@users.noreply.github.com>
2022-10-29 21:40:43 +0300

    updated api

--- Wordle Game/api.py
+++ Wordle Game/api.py
@@ -2,8 +2,18 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((600, 400))
-pygame.display.set_caption("Wordle")
+
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Wordle!")
+icon = pygame.image.load("assets/icon.png")
+pygame.display.set_icon(icon)
+
+background = pygame.image.load("assets/tiles.png")
+background_rect = background.get_rect(center = (400, 300))
+
+Green = "#538d4e"
+Yellow = "#b59f3b"
+Grey = "#3a3a3c"
 
 def main():
       clock = pygame.time.Clock()
@@ -13,6 +23,8 @@ def main():
                         pygame.quit()
                         exit()
             #draw all the elements required to make a wordle api
+            screen.fill("#121213")
+            screen.blit(background, background_rect)
             #update everything
             pygame.display.update()
             clock.tick(60)
