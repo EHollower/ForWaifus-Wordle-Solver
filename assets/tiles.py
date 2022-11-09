61941c5ba6cc84e4a11b10dd9c389ad987b8dac9
EHollower <76710132+EHollower@users.noreply.github.com>
2022-11-09 10:29:56 +0200

    draw tile class

--- assets/tiles.py
+++ assets/tiles.py
@@ -7,37 +7,49 @@ class Tiles:
       #will initialize the coordinates on the screen of each tile
       def __init__(self, width, height):
             self.width, self.height = width, height
-            self.mat = [[[0, 0] for i in range(width)] for i in range(height)]
+            self.coord = [[[0, 0] for i in range(width)] for i in range(height)]
 
             for i in range(self.height):
                   for j in range(self.width):
                         if i == 0 and j == 0:
-                              self.mat[i][j][0], self.mat[i][j][1] = 210, 100
+                              self.coord[i][j][0], self.coord[i][j][1] = 210, 100
                         elif i > 0 and j == 0:
-                              self.mat[i][j][0], self.mat[i][j][1] = self.mat[i - 1][j][0], self.mat[i - 1][j][1] + 65
+                              self.coord[i][j][0], self.coord[i][j][1] = self.coord[i - 1][j][0], self.coord[i - 1][j][1] + 65
                         else:
-                              self.mat[i][j][0], self.mat[i][j][1] = self.mat[i][j - 1][0] + 65, self.mat[i][j - 1][1]
+                              self.coord[i][j][0], self.coord[i][j][1] = self.coord[i][j - 1][0] + 65, self.coord[i][j - 1][1]
 
       #function will be used to draw each tile of the Wordle table
       def draw(self, screen, color):
             for i in range(self.height):
                   for j in range(self.width):
-                        x, y = self.mat[i][j][0], self.mat[i][j][1]
+                        x, y = self.coord[i][j][0], self.coord[i][j][1]
                         tile = pygame.Rect(x, y, 60, 60)
-                        if color[i][j] == colors_arr[6]:
+                        if color[i][j] == colors_arr[7] or color[i][j] == colors_arr[6]:
                               pygame.draw.rect(screen, color[i][j], tile, 2)
                               continue
                         pygame.draw.rect(screen, color[i][j], tile)
 
-
-      def draw_animation(self, i, j, screen, color):
+      #function will be used to make and animation when a letter is pressed
+      def draw_insert_animation(self, ic, jc, screen, color, img):
             dx, dy = 60, 60
-            for p in range(5):
-                  dx += 3
-                  dy += 3
-                  x, y = self.mat[i][j][0] - p * 2, self.mat[i][j][1] - p * 2
-                  tile = pygame.Rect(x, y, dx, dy)
+            for rep in range(5):
                   screen.fill(colors_arr[1])
-                  pygame.draw.rect(screen, color[i][j], tile, 2)
+                  screen.blit(img, (310, 25))
+                  for i in range(self.height):
+                        for j in range(self.width):
+                              if i == ic and j == jc:
+                                    dx += 2
+                                    dy += 2
+                                    x, y = self.coord[i][j][0] - rep * 2, self.coord[i][j][1] - rep * 2
+                                    tile = pygame.Rect(x, y, dx, dy)
+                                    pygame.draw.rect(screen, color[i][j], tile, 2)
+                                    continue
+                              x, y = self.coord[i][j][0], self.coord[i][j][1]
+                              tile = pygame.Rect(x, y, 60, 60)
+                              if color[i][j] == colors_arr[7] or color[i][j] == colors_arr[6]:
+                                    pygame.draw.rect(screen, color[i][j], tile, 2)
+                                    continue
+                              pygame.draw.rect(screen, color[i][j], tile)
+
                   pygame.display.update()
                   time.sleep(0.01)
