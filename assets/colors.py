9000a6787516e851b2c12ef4976e64d0e7fb1763
EHollower <76710132+EHollower@users.noreply.github.com>
2022-11-09 10:29:36 +0200

    Wordle colors

--- assets/colors.py
+++ assets/colors.py
@@ -1,11 +1,12 @@
 #colors thats will be used trought the playing of the game
 Green = "#538d4e"
 Yellow = "#b59f3b"
-Grey = "#3a3a3c"
-Greyy = "#3a3a3d"
+Grey1 = "#3a3a3c"
+Grey2 = "#565758"
+Grey3 = "#3a3a3d"
 Black = "#121213"
 White = "#ffffff"
 Red = "#cc0000"
 
 #make an array of colors
-colors_arr = [White, Black, Green, Yellow, Red, Grey, Greyy]
+colors_arr = [White, Black, Green, Yellow, Red, Grey1, Grey2, Grey3]
