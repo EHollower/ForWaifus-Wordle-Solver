fec4d9d7e0e60512b2ec492958bf07e87f4a38c8
EHollower <76710132+EHollower@users.noreply.github.com>
2022-11-18 22:08:12 +0200

    game of wordle that does not interact with solve.exe

--- shell.py
+++ shell.py
@@ -1,109 +1,88 @@
+"""
+The following program is a standard implementaion of the wordle game without the solver
+This program was made first to test some interactions/animations more easly than with game.py 
+"""
+
 import pygame
 import random
+
+from assets.animations import *
+from assets.interface import *
+from assets.letters import *
+from assets.colors import *
+from assets.tiles import *
+
 from dictionary import wordle_dictionary
-from assets.colors import colors_arr, colorTemp, colorTile
-from assets.tiles import Tiles
-from assets.letters import Letters
-from assets.animations import Animations
-from sys import exit
 
 #This will initialize the Wordle game/game window
 pygame.init()
-screen = pygame.display.set_mode((800, 700))
-pygame.display.set_caption("Wordle!")
-icon = pygame.image.load("assets/imgs/icon.png")
-pygame.display.set_icon(icon)
+configs = GameConfig()
 
-#title of the game
-title = pygame.image.load("assets/imgs/title.png").convert()
-#font of each letter
-font = pygame.font.SysFont('Clear Sans', 40)
-
-#classes the will be used to draw/animate the Wordle game
-animations = Animations(screen, title, font)
+#classes the will be used to draw/animate the wordle game
+animations = Animations()
 tiles = Tiles(5, 6)
 letters = Letters(tiles.coord, 5, 6)
 
-run = True
-
-def outcome(wordle, guess):
-      global run
-      n = len(wordle)
-      for j in range(n):
-            colorTemp[j] = colors_arr[7]
-            if guess[j] == wordle[j]:
-                  colorTemp[j] = colors_arr[2]
-      
-      for j in range(n):
-            if guess[j] in wordle and colorTemp[j] == colors_arr[7]: colorTemp[j] = colors_arr[3]
-            elif colorTemp[j] == colors_arr[7]: colorTemp[j] = colors_arr[5]
-
-      if colorTemp.count(colors_arr[2]) == n:
-            run = False
-
 def main():
       wordle = random.choice(wordle_dictionary)
-      print(wordle)
-      clock = pygame.time.Clock()
+
+      run, score = True, 0
+      animations.turn_on()
       last = 0
       while run:
-            letters.permute()
-            animations.screen_init()
-            tiles.draw_table(screen)
-            letters.draw(screen, font)
+            configs.screen_init_(score, run)
+
+            letters.permute() #permutes the table up by one row if the number of guesses is bigger than 6
+            tiles.draw_table(configs.screen) #draws the table of guesses
+            letters.draw(configs.screen, configs.font) #draws each letter inside the table of guesses
+
             for event in pygame.event.get():
+                  #close the program
                   if event.type == pygame.QUIT:
                         pygame.quit()
                         exit()
                   
                   if event.type ==  pygame.KEYDOWN:
+                        #delete letter
                         if event.key == pygame.K_BACKSPACE:
                               letters.delete_letter()
                         
                         if event.key == pygame.K_RETURN:
                               now = pygame.time.get_ticks()
                               
+                              #verify if the guess made is a word contained in the dictionary of words that we have
                               if "".join(letters.str[letters.y]) in wordle_dictionary:
-                                    outcome(wordle, letters.str[letters.y])
-                                    animations.outcome_animation(tiles, letters)
+                                    score += 1
+                                    run = outcome(wordle, letters.str[letters.y])
+                                    animations.outcome_animation(configs, tiles, letters, score)
                                     letters.enter_guess()
                                     continue
                                     
                               if now - last >= 300:
-                                    animations.worng_animation(tiles, letters)
+                                    animations.worng_animation(configs, tiles, letters, score)
                                     last = pygame.time.get_ticks()
 
                         key_pressed = event.unicode.upper()
                         if key_pressed in "QWERTYUIOPASDFGHJKLZXCVBNM" and key_pressed != "":
-                              animations.insert_animation(tiles, letters)
+                              animations.insert_animation(configs, tiles, letters, score)
                               letters.insert_letter(key_pressed)
                         
-
-            pygame.display.update()
-            clock.tick(60)
+            configs.update_screen()
+            
 
       while True:
-            screen.fill(colors_arr[1])
-            screen.blit(title, (300, 25))
+            configs.screen_init_(score, run)
 
-            textsur = font.render("Total number of guesses:", True, colors_arr[0])
-            textrect = textsur.get_rect()
-            textrect.center = (370, 530)
-            screen.blit(textsur, textrect)
+            tiles.draw_table(configs.screen)
+            letters.draw(configs.screen, configs.font)
 
-            cntsur = font.render(str(animations.count), True, colors_arr[0])
-            cntrect = cntsur.get_rect()
-            cntrect.center = (370, 570)
-            screen.blit(cntsur, cntrect)
-
-            tiles.draw_table(screen)
-            letters.draw(screen, font)
             for event in pygame.event.get():
+                  #close the program
                   if event.type == pygame.QUIT:
                         pygame.quit()
                         exit()
-            pygame.display.update()
-            clock.tick(60)
+
+            configs.update_screen()
 
 if __name__ == '__main__':
       main()
