d8d22a75caf9cc2d4bcf4e8dfe9d43fa29d2e63a
EHollower <76710132+EHollower@users.noreply.github.com>
2022-11-18 22:02:41 +0200

    class that handles animations of the game

--- assets/animations.py
+++ assets/animations.py
@@ -1,111 +1,115 @@
 import pygame
-import subprocess
-import os
-from assets.colors import colors_arr, colorTemp, colorTile
-from sys import exit
+from assets.colors import colorTemp, colorTile
 
 #this class will be used to draw the animation of the Worlde game
+#the animations can be turned off
 class Animations:
-      def __init__(self, screen, title, font):
-            self.screen = screen
-            self.title = title
-            self.font = font
-            self.count = 0
-            self.p = subprocess.Popen(["solver"], shell=True)
+      #turn on/off animations
+      def turn_off(self):
+            self.active = False
+      
+      def turn_on(self):
+            self.active = True
 
-      def screen_init(self):
-            self.screen.fill(colors_arr[1])
-            self.screen.blit(self.title, (300, 25))
-
-            textsur = self.font.render("Number of guesses:", True, colors_arr[0])
-            textrect = textsur.get_rect()
-            textrect.center = (370, 530)
-            self.screen.blit(textsur, textrect)
-            
-            cntsur = self.font.render(str(self.count), True, colors_arr[0])
-            cntrect = cntsur.get_rect()
-            cntrect.center = (370, 570)
-            self.screen.blit(cntsur, cntrect)
-
-      def event(self):
-             for event in pygame.event.get():
-                  if event.type == pygame.QUIT:
-                        g = open("communication.txt", "w")
-                        g.write("---")
-                        g.close()
-                        pygame.quit()
-                        exit()
-
-      #this funtion will be used to draw the animation when you insert a new letter
-      def insert_animation(self, tiles, letters):
+      """
+      this function will be used to draw the animation when you insert a new letter
+      to describe the process of how we draw the animation:
+            we basically select the current position we intserted a letter to and expent the rectagle that 
+      """
+      def insert_animation(self, configs, tiles, letters, score):
             height, width = tiles.height, tiles.width
             lx, ly = letters.x, letters.y
-            #zoom out
+            
+            #zoom out/expand cell
             for rep in range(10):
-                  self.event()   
-                  self.screen_init()
+                  #we skip the animation if we sought so
+                  if self.active == False:
+                        break
+
+                  configs.event_handle_solver()
+                  configs.screen_init_(score, True)
+
                   for i in range(height):
                         for j in range(width):
                               x, y = tiles.coord[i][j][0], tiles.coord[i][j][1]
                               color = colorTile[i][j]
                               if i == ly and j == lx:
-                                    tiles.draw_tile(self.screen, x - 5, y - 3, 60 + rep + 1, color)
+                                    #coordinates were set in such a way that it just looks good to the naked eye when we draw the animation
+                                    tiles.draw_tile(configs.screen, x - 5, y - 3, 60 + rep + 1, color)
                                     continue
-                              tiles.draw_tile(self.screen, x, y, 60, color)
+                              tiles.draw_tile(configs.screen, x, y, 60, color)
 
-                  letters.draw(self.screen, self.font)
-                  pygame.display.update()
-                  pygame.time.Clock().tick(200)
+                  letters.draw(configs.screen, configs.font)
+                  configs.update_screen(120)
 
-      #this will draw the animation when a word is guessed
-      def outcome_animation(self, tiles, letters):
-            self.count += 1
+      """
+      this function will be used to draw the animation when you insert a new guess
+      to describe the process of how we draw the animation:
+            we basically go from the first to the last cell in which we introduced the word and firstly we shrink the cell
+            and then we expand the cell with its coresponding color after the guess 
+      """
+      def outcome_animation(self, configs, tiles, letters, score):
             height, width = tiles.height, tiles.width
             ly = letters.y
             #zoom in
             for lx in range(width):
+                  #we skip the animation if we sought so
+                  if self.active == False:
+                        for j in range(width):
+                              colorTile[ly][j] = colorTemp[j]
+                        break
+
+                  #zoom in/shrink cell
                   for rep in range(30):
-                        self.event() 
-                        self.screen_init()
+                        configs.event_handle_solver()
+                        configs.screen_init_(score, True)
+
                         for i in range(height):
                               for j in range(width):
                                     x, y = tiles.coord[i][j][0], tiles.coord[i][j][1]
                                     color = colorTile[i][j]
                                     if i == ly and j == lx:
-                                          tiles.draw_tile(self.screen, x + rep, y + rep + 1, 60 - (rep + 1) * 2, color)
+                                          #coordinates were set in such a way that it just looks good to the naked eye when we draw the animation
+                                          tiles.draw_tile(configs.screen, x + rep, y + rep + 1, 60 - (rep + 1) * 2, color)
                                           continue
-                                    tiles.draw_tile(self.screen, x, y, 60, color)
+                                    tiles.draw_tile(configs.screen, x, y, 60, color)
 
-                        letters.draw(self.screen, self.font)
-                        pygame.display.update()
-                        pygame.time.Clock().tick(200)
-
-
+                        letters.draw(configs.screen, configs.font)
+                        configs.update_screen(120)
                   
-                  #zoom out
+                  #zoom out/expand cell
                   for rep in range(30):
-                        self.event()
-                        self.screen_init()
+                        configs.event_handle_solver()
+                        configs.screen_init_(score, True)
+
                         for i in range(height):
                               for j in range(width):
                                     x, y = tiles.coord[i][j][0], tiles.coord[i][j][1]
                                     txt = letters.str[i][j]
                                     color = colorTile[i][j]
                                     if i == ly and j == lx:
-                                          tiles.draw_tile(self.screen, x + (29 - rep + 1), y, (rep + 1) * 2, colorTemp[j])
+                                          #coordinates were set in such a way that it just looks good to the naked eye when we draw the animation
+                                          tiles.draw_tile(configs.screen, x + (29 - rep + 1), y, (rep + 1) * 2, colorTemp[j])
                                           colorTile[i][j] = colorTemp[j]
-                                          letters.draw_letter(self.screen, x, y, txt, self.font)
+                                          letters.draw_letter(configs.screen, x, y, txt, configs.font)
                                           continue
-                                    tiles.draw_tile(self.screen, x, y, 60, color)
-                                    letters.draw_letter(self.screen, x, y, txt, self.font)
 
-                        pygame.display.update()
-                        pygame.time.Clock().tick(200)
-                  pygame.time.Clock().tick(200)
+                                    tiles.draw_tile(configs.screen, x, y, 60, color)
+                                    letters.draw_letter(configs.screen, x, y, txt, configs.font)
+
+                        configs.update_screen(120)
+
+                  configs.update_screen(120)
 
 
-      #used for drawing the aniamtion when an invalid move is made troughout 
-      def worng_animation(self, tiles, letters):
+      #used for drawing the aniamtion when an invalid word is being introduced as a guess
+      """
+      this animation is not used in the game.py but it is used in shell.py
+      to describe the basic process of how we draw the animation:
+            we basically shift the position of the cells form left to right and the position of the letters in the cells as well using
+            the matrix offset
+      """
+      def worng_animation(self, configs, tiles, letters, score):
             height, width = tiles.height, tiles.width
             ly = letters.y
             offset = [[[0, 0] for i in range(width)] for i in range(height)]
@@ -113,34 +117,33 @@ class Animations:
             for rep in range(3):
                   #shift left
                   for rep in range(10):
-                        self.event()
-                        self.screen_init()
+                        configs.event_handle_solver()
+                        configs.screen_init_(score, True)
                         for i in range(height):
                               for j in range(width):
                                     x, y = tiles.coord[i][j][0], tiles.coord[i][j][1]
                                     color = colorTile[i][j]
                                     if i == ly:
                                           offset[i][j][0], offset[i][j][1] = rep + 1, 0
-                                          tiles.draw_tile(self.screen, x + (rep + 1), y, 60, color)
+                                          tiles.draw_tile(configs.screen, x + (rep + 1), y, 60, color)
                                           continue
                                     offset[i][j][0], offset[i][j][1] = 0, 0
-                                    tiles.draw_tile(self.screen, x, y, 60, colorTile[i][j])
+                                    tiles.draw_tile(configs.screen, x, y, 60, colorTile[i][j])
                         
-                        letters.draw_offset(self.screen, self.font, offset)
-                        pygame.display.update()
-                        pygame.time.Clock().tick(200)
+                        letters.draw_offset(configs.screen, configs.font, offset)
+                        configs.update_screen(120)
 
                   #shift right
                   for rep in range(10):
-                        self.screen_init()
+                        configs.event_handle_solver()
+                        configs.screen_init_(score, True)
                         for i in range(height):
                               for j in range(width):
                                     if i == letters.y:
                                           offset[i][j][0], offset[i][j][1] = -(rep + 1), 0 
-                                          tiles.draw_tile(self.screen, tiles.coord[i][j][0] - (rep + 1), tiles.coord[i][j][1], 60, colorTile[i][j])
+                                          tiles.draw_tile(configs.screen, tiles.coord[i][j][0] - (rep + 1), tiles.coord[i][j][1], 60, colorTile[i][j])
                                           continue
                                     offset[i][j][0], offset[i][j][1] = 0, 0
-                                    tiles.draw_tile(self.screen, tiles.coord[i][j][0], tiles.coord[i][j][1], 60, colorTile[i][j])
-                        letters.draw_offset(self.screen, self.font, offset)
-                        pygame.display.update()
-                        pygame.time.Clock().tick(200)
+                                    tiles.draw_tile(configs.screen, tiles.coord[i][j][0], tiles.coord[i][j][1], 60, colorTile[i][j])
+                        letters.draw_offset(configs.screen, configs.font, offset)
+                        configs.update_screen(120)
